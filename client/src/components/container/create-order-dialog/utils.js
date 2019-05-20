dd0b914189bfd3f2c6ef47dc637b7c0e6a711c0a
Jordan Henderson <hendersonj219@gmail.com>
2019-05-19 23:08:51 -0700

    Move create order dialog utils

---
+++ client/src/components/container/create-order-dialog/utils.js
@@ -0,0 +1,33 @@
+export const dateIsValid = (dateStr) => {
+  const dateRegex = /\d{4}-\d{2}-\d{2}/g;
+  return dateRegex.test(dateStr);
+}
+
+export const isUUID = (str) => {
+  const UUIDRegex = /[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/g
+  return UUIDRegex.test(str);
+}
+export const validateForm = (values = {}) => {
+  const errors = {};
+  const { shipDate, cases, brewMethodID, coffeeID, caseTypeID, notes } = values; 
+  const MAX_NOTES_LENGTH = 255;
+  if (!shipDate || !dateIsValid(shipDate)) {
+    errors.shipDate = 'Invalid Ship Date';
+  }
+  if (!cases || cases < 1) {
+    errors.cases = 'Invalid number of cases';
+  }
+  if (!brewMethodID || !isUUID(brewMethodID)) {
+    errors.brewMethodID = "Brew method is required";
+  }
+  if (!coffeeID || !isUUID(coffeeID)) {
+    errors.coffeeID = "Coffee type is required";
+  }
+  if (!caseTypeID || !isUUID(caseTypeID)) {
+    errors.caseTypeID = "Packets per case is required";
+  }
+  if (notes.length > MAX_NOTES_LENGTH) {
+    errors.notes = "Notes must be shorter than 255 characters"
+  }
+  return errors;
+}
